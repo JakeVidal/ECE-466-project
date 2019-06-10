37bf7bb53d1042bbf1bfc9cc6f58ccb6437d803b
Jake Vidal <jrvidal@ugls22.ece.uvic.ca>
2019-06-10 13:03:01 -0700

    wrote testbench logic for memory interfacing

--- Assignment 2/Assignment 2 Q1/testbench.h
+++ Assignment 2/Assignment 2 Q1/testbench.h
@@ -19,25 +19,72 @@ SC_MODULE(testbench) {
     sc_in < sc_uint<2> > comm;
     sc_in <bool> new_comm; // Input port signaling “start”
     sc_out <bool> complete; // Output port signaling “done”
-    
+ 
     sc_in_clk clk;
-    sc_in <bool> reset;
 
     void test_process() {
 	sc_lv <8> data_s;  
 	while (true) {
 	    if (reset.read() == true) new_comm.write(false);
 	    else {
-		data.write(0xFF); //set-up for command
+		data.write(0xFF); // set-up for WTBYT command
 		addr.write(0xFF);
-		comm.write(RDBYT);
-		new_comm.write(true); //send command to memory
+		comm.write(WTBYT);
+		wait();
+		new_comm.write(true); // send command to memory
 		while (complete.read() == false) {
-		    if (reset.read() == true) break;
 		    wait();
 		}
-		data_s = data.read(); //read result
+		data.write(Z);
 		new_comm.write(false);
+		wait();
+		
+		addr.write(0xFF); // set-up for RDBYT command
+		comm.write(RDBYT);
+		wait();
+		new_comm.write(true); // send command to memory
+		while (complete.read() == false) {
+		    wait();
+		}
+		data_s = data.read();
+		new_comm.write(false);
+		wait();
+		
+		data.write(0xEE); // set-up for WTBLK command
+		addr.write(0x00);
+		comm.write(WTBLK);
+		wait();
+		new_comm.write(true);
+		wait();
+		data.write(0xDD);
+		wait();
+		data.write(0xCC);
+		wait();
+		data.write(0xBB);
+		while (complete.read() == false) {
+		    wait();
+		}
+		data.write(Z);
+		new_comm.write(false);
+		wait();
+		
+		addr.write(0x00); // set-up for RDBLK command
+		comm.write(RDBLK);
+		wait();
+		new_comm.write(true); // send command to memory
+		wait();
+		data_s = data.read();
+		wait();
+		data_s = data.read();
+		wait();
+		data_s = data.read();
+		wait();
+		data_s = data.read();
+		while (complete.read() == false) {
+		    wait();
+		}
+		new_comm.write(false);
+		wait();
 	    }
 	}
     }
