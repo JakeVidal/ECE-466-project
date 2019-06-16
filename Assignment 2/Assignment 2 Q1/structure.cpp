a34021ecba435050af90a3db7c721f863512e3a7
JakeVidal <jrvidal@shaw.ca>
2019-06-15 18:51:18 -0700

    completed rough draft of A2Q2

--- Assignment 2/Assignment 2 Q1/structure.cpp
+++ Assignment 2/Assignment 2 Q1/structure.cpp
@@ -4,40 +4,11 @@
 #include <iostream>
 #include <iomanip>
 
-int sc_main(int argc, char* argv[]){
-	sc_clock TestClk("CLKTEST", 10, SC_NS, 0.5, 1, SC_NS);
-	sc_signal <bool> RST;
-	sc_signal <float> XIN, YOUT;
-
-        filter2 DUT("filter2");
-	DUT.CLK(TestClk); DUT.RST(RST); DUT.XIN(XIN); DUT.YOUT(YOUT);
-
-	stimulus STIM("stimulus");
-	STIM.CLK(TestClk); STIM.RST(RST); STIM.XIN(XIN);
-
-	monitor MON("monitor");
-	MON.CLK(TestClk); MON.RST(RST); MON.XIN(XIN); MON.YOUT(YOUT);
-
-	sc_trace_file* TF = sc_create_vcd_trace_file("filter_traces");
-	TF->set_time_unit(1, SC_NS);
-	sc_trace(TF, RST, "RST");
-	sc_trace(TF, XIN, "X");
-	sc_trace(TF, YOUT, "Y");
-
-	sc_start(210, SC_NS);
-        sc_close_vcd_trace_file(TF);
-	return 0;
-	
-};
-
 int sc_main(int argc, char* argv[])
 {
-    // Memory module instance:
     mem_ctrl MC("MC");
-    
-    // Your testbench module(s) instances:
-    ...
-
+    testbench TB("TB");
+    sc_clock TestClk("CLKTEST", 10, SC_NS, 0.5, 1, SC_NS);
 
     // Internal signals:
     sc_signal <bool> reset, new_comm, complete;
@@ -46,7 +17,6 @@ int sc_main(int argc, char* argv[])
     sc_signal_rv <8> data;
     // _rv needed because of multiple drivers
   
-
     // Interconnects:
     MC.clk(clk);
     MC.reset(reset);
@@ -56,10 +26,26 @@ int sc_main(int argc, char* argv[])
     MC.new_comm(new_comm);
     MC.complete(complete);
 
-    ...
+    TB.clk(clk);
+    TB.reset(reset);
+    TB.data(data);
+    TB.addr(addr);
+    TB.comm(comm);
+    TB.new_comm(new_comm);
+    TB.complete(complete);
 
+    sc_trace_file* TF = sc_create_vcd_trace_file("memory_traces");
+    TF->set_time_unit(1, SC_NS);
+    sc_trace(TF, clk, "clk");
+    sc_trace(TF, reset, "reset");
+    sc_trace(TF, data, "data");
+    sc_trace(TF, addr, "addr");
+    sc_trace(TF, comm, "comm");
+    sc_trace(TF, new_comm, "new_comm");
+    sc_trace(TF, complete, "complete");
 
-    // Rest of sc_main ...
+    sc_start(210, SC_NS);
+    sc_close_vcd_trace_file(TF);
 
     return 0;
 }
--- Assignment 2/Assignment 2 Q1/testbench.h
+++ Assignment 2/Assignment 2 Q1/testbench.h
@@ -10,86 +10,87 @@
 using namespace std;
 
 static sc_logic Z[8] = {SC_LOGIC_Z, SC_LOGIC_Z, SC_LOGIC_Z, SC_LOGIC_Z,
-		        SC_LOGIC_Z, SC_LOGIC_Z, SC_LOGIC_Z, SC_LOGIC_Z};
+		        		SC_LOGIC_Z, SC_LOGIC_Z, SC_LOGIC_Z, SC_LOGIC_Z};
 			
 SC_MODULE(testbench) {
 
     sc_inout <sc_lv<8> > data; // Bidirectional port of logic-vector type
     sc_in < sc_uint<8> > addr;
     sc_in < sc_uint<2> > comm;
+    sc_in <bool> reset;
     sc_in <bool> new_comm; // Input port signaling “start”
     sc_out <bool> complete; // Output port signaling “done”
  
     sc_in_clk clk;
 
     void test_process() {
-	sc_lv <8> data_s;  
-	while (true) {
-	    if (reset.read() == true) new_comm.write(false);
-	    else {
-		data.write(0xFF); // set-up for WTBYT command
-		addr.write(0xFF);
-		comm.write(WTBYT);
-		wait();
-		new_comm.write(true); // send command to memory
-		while (complete.read() == false) {
-		    wait();
+		sc_lv <8> data_s;  
+		while (true) {
+		    if (reset.read() == true) new_comm.write(false);
+		    else {
+			data.write(0xFF); // set-up for WTBYT command
+			addr.write(0xFF);
+			comm.write(WTBYT);
+			wait();
+			new_comm.write(true); // send command to memory
+			while (complete.read() == false) {
+			    wait();
+			}
+			data.write(Z);
+			new_comm.write(false);
+			wait();
+			
+			addr.write(0xFF); // set-up for RDBYT command
+			comm.write(RDBYT);
+			wait();
+			new_comm.write(true); // send command to memory
+			while (complete.read() == false) {
+			    wait();
+			}
+			data_s = data.read();
+			new_comm.write(false);
+			wait();
+			
+			data.write(0xEE); // set-up for WTBLK command
+			addr.write(0x00);
+			comm.write(WTBLK);
+			wait();
+			new_comm.write(true);
+			wait();
+			data.write(0xDD);
+			wait();
+			data.write(0xCC);
+			wait();
+			data.write(0xBB);
+			while (complete.read() == false) {
+			    wait();
+			}
+			data.write(Z);
+			new_comm.write(false);
+			wait();
+			
+			addr.write(0x00); // set-up for RDBLK command
+			comm.write(RDBLK);
+			wait();
+			new_comm.write(true); // send command to memory
+			wait();
+			data_s = data.read();
+			wait();
+			data_s = data.read();
+			wait();
+			data_s = data.read();
+			wait();
+			data_s = data.read();
+			while (complete.read() == false) {
+			    wait();
+			}
+			new_comm.write(false);
+			wait();
+		    }
 		}
-		data.write(Z);
-		new_comm.write(false);
-		wait();
-		
-		addr.write(0xFF); // set-up for RDBYT command
-		comm.write(RDBYT);
-		wait();
-		new_comm.write(true); // send command to memory
-		while (complete.read() == false) {
-		    wait();
-		}
-		data_s = data.read();
-		new_comm.write(false);
-		wait();
-		
-		data.write(0xEE); // set-up for WTBLK command
-		addr.write(0x00);
-		comm.write(WTBLK);
-		wait();
-		new_comm.write(true);
-		wait();
-		data.write(0xDD);
-		wait();
-		data.write(0xCC);
-		wait();
-		data.write(0xBB);
-		while (complete.read() == false) {
-		    wait();
-		}
-		data.write(Z);
-		new_comm.write(false);
-		wait();
-		
-		addr.write(0x00); // set-up for RDBLK command
-		comm.write(RDBLK);
-		wait();
-		new_comm.write(true); // send command to memory
-		wait();
-		data_s = data.read();
-		wait();
-		data_s = data.read();
-		wait();
-		data_s = data.read();
-		wait();
-		data_s = data.read();
-		while (complete.read() == false) {
-		    wait();
-		}
-		new_comm.write(false);
-		wait();
-	    }
-	}
     }
     
     SC_CTOR(testbench) { 
-	SC_CTHREAD(test_process, clk.pos()); 
+		SC_CTHREAD(test_process, clk.pos()); 
     }
 };
