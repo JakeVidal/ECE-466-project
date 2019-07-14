262c95a29488669096f7f382f207f82dfcd52591
JakeVidal <jrvidal@shaw.ca>
2019-07-14 13:15:23 -0700

    added structural diagram and module files

---
+++ Project/modules.cpp
@@ -0,0 +1,8 @@
+#include "systemc.h"
+#include "digit.h"
+#include "modules.h"
+
+/*
+void adder::process_hw_add()
+{
+*/
---
+++ Project/modules.h
@@ -0,0 +1,28 @@
+#include "systemc.h"
+#include "digit.h"
+
+#ifndef _MODULES_H_
+#define _MODULES_H_ 1
+
+/*
+SC_MODULE (dh_hw_mult)
+{
+  sc_in_clk clk;
+  sc_in<bool> hw_mult_enable; 
+  sc_in<NN_DIGIT> in_data_1;
+  sc_in<NN_DIGIT> in_data_2;
+  sc_out<NN_DIGIT> out_data_low;
+  sc_out<NN_DIGIT> out_data_high;
+  sc_out<bool> hw_mult_done;
+
+  void process_hw_mult();
+  
+  SC_CTOR (dh_hw_mult)
+  {
+    SC_THREAD ();
+  }
+  
+};
+*/
+
+#endif /* end _MODULES_H_ */
